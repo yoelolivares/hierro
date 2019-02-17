4f68c4d8529586febccaaf07b1aee8d706d48213
yoel Olivares <dev.yllvrs@gmail.com>
2019-02-17 14:16:55 -0600

    committed new updates

--- js/main.js
+++ js/main.js
@@ -2,50 +2,36 @@
 
 
 
-$(document).ready(function() {
 
-    $('.btn-1').click(function() {
-        $('.item-card-1').animate({height: 'toggle', display: 'block'});
-    });  
-    $('.btn-2').click(function() {
-        $('.item-card-2').animate({height: 'toggle', display: 'block'});
-    });
-    $('.btn-3').click(function() {
-        $('.item-card-3').animate({height: 'toggle', display: 'block'});
-    });
 
-    $('.btn-left').click(function() {
-        $('.item-card-1').animate({height: 'toggle', display: 'none'});
-    });    
-    $('.btn-middle').click(function() {
-        $('.item-card-2').animate({height: 'toggle', display: 'none'});
-    });
-    $('.btn-right').click(function() {
-        $('.item-card-3').animate({height: 'toggle', display: 'none'});
-    });
-        
 
-    //menu-btn toggle
-    $('.nav-btn').click(function() {
-        $('.main-nav').toggleClass('visible');
-        $('.line').toggleClass('switch');
-    });
+
+
+// $(document).ready(function() {
     
-    $("#wrapper").click( function() {
-        $(".icon").toggleClass("close");
-    });
+//     $('.js-animateUp').on('click', function(event) {
+        
+//         event.currentTarget = $(this);
+//             $('.item-description-card').animate({display: 'block', height: '100%'});
+//         console.log(selectedCard);
+//     });    
+//     $('.minimize-btn').on('click', function () {
+//         $('.item-description-card').animate({display: 'block', height: '0'});
+//     }) 
 
-    //show cart
-    $('.cart-icon').click(function() {
-        $('.shopping-cart').toggleClass("show-cart");
-    });
+//     //menu-btn toggle
+//     $('.nav-btn').click(function() {
+//         $('.main-nav').toggleClass('visible');$('.item-description-card').animate({height: '100%', display: 'block'});
+//         $('.line').toggleClass('switch');
+//     });
+    
+//     $("#wrapper").click( function() {
+//         $(".icon").toggleClass("close");
+//     });
 
-});
+//     //show cart
+//     $('.cart-icon').click(function() {
+//         $('.shopping-cart').toggleClass("show-cart");
+//     });
 
-
-
-
-var striker = 3249.99;
-var bigBoy = 1999.99;
-var veneno = 1449.99;
-
+// });
